e5f84a97a7b75abbc62e4335ba1725a7b80151a6
Elber <elbercastillomesa@gmail.com>
2019-05-22 22:57:47 -0500

    setup layout components -> Navbar.js, SignInLinks.js, SignOutLinks.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
 
   render(){
     return(
       <BrowserRouter>
-                      <div className="App">
-                        <h1>MarioPlan</h1>
-                      </div>
+        <div className="App">
+          <Navbar />
+        </div>
       </BrowserRouter>
     )
   }
