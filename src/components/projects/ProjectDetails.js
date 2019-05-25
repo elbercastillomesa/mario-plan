26d3c5da3b77ff886f970da4b9a45c708a0d185a
Elber <elbercastillomesa@gmail.com>
2019-05-24 22:09:19 -0500

    import id props to Project Details component and show it as part of title

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function ProjectDetails() {
+const ProjectDetails = (props)  => {
+
+    const id = props.match.params.id;
+
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <div className="card-title">Project Title</div>
+                    <div className="card-title">Project Title - {id}</div>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nesciunt suscipit aliquid enim quod laboriosam sunt natus repudiandae tempore velit, nostrum fugit ad, deserunt, sit officiis id. Necessitatibus, harum qui?</p>                
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
