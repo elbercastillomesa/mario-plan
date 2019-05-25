f88c04f4ac3fbc162ea2d03e61b54cd7d196b55c
Elber <elbercastillomesa@gmail.com>
2019-05-24 21:37:44 -0500

    create Project Details component using VS extension and command 'ref'+tab-key This is a template component of the details of every project

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ProjectDetails() {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <div className="card-title">Project Title</div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nesciunt suscipit aliquid enim quod laboriosam sunt natus repudiandae tempore velit, nostrum fugit ad, deserunt, sit officiis id. Necessitatibus, harum qui?</p>                
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by EPCM</div>
+                    <div>24th May, 21:36</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails;
