756c413d91287272cdbf8a1691b6f280d39be4ab
Elber Castillo Mesa <elber.castillo@correounivalle.edu.co>
2019-06-05 10:59:32 -0500

    fix ProjectList.js error, each 'link' child must have a unique key, so is taken from ProjectSummary component to Link

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -8,8 +8,8 @@ const ProjectList = ({projects}) => {
             {projects && projects.map( //The ampersan is used to evaluate if there's projects to implement the map
                 project => {
                     return(
-                        <Link to={'/project/' + project.id}>
-                            <ProjectSummary project={project} key={project.id} />
+                        <Link to={'/project/' + project.id} key={project.id}>
+                            <ProjectSummary project={project} />
                         </Link>
                     )
                 }
