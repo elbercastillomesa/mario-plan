f82bc4f9a6c26ab107595a1dd1af34e04818615f
Elber Castillo Mesa <elber.castillo@correounivalle.edu.co>
2019-06-05 17:18:40 -0500

    routes guarded to avoid once logued out can insert data and viceversa

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,13 +4,13 @@ import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
 
     render(){
-        //console.log(this.props);
-
-        const { projects } = this.props
+        const { projects, auth } = this.props;        
+        if(!auth.uid) return <Redirect to ='/signin' />
 
         return(
             <div className="dashboard container">
@@ -30,7 +30,8 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
     console.log(state);
     return{
-        projects: state.firestore.ordered.projects  // now the data is imported from google's firestore
+        projects: state.firestore.ordered.projects,  // now the data is imported from google's firestore
+        auth: state.firebase.auth
     }
 }
 
