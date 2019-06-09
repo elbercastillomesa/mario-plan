61a3eb59deeceea58197e5ea60a03bbe7112467b
Elber <elbercastillomesa@gmail.com>
2019-06-08 20:05:36 -0500

    RUN BUILD and moved files to 'dist' folder

---
+++ dist/precache-manifest.7ace54f2e5ab8855553817be805c72a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2d1d3fcf8a13e07756b78fe7d1fee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48407602724aca8cf91",
+    "url": "/static/css/main.b6bf6e76.chunk.css"
+  },
+  {
+    "revision": "953f368bef08e3e087d9",
+    "url": "/static/js/2.a39884e2.chunk.js"
+  },
+  {
+    "revision": "f48407602724aca8cf91",
+    "url": "/static/js/main.88f9aa62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
